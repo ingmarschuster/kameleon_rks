Fixed Scaling bug in AdaptiveMetropolis

kameleon_rks/AdaptiveMetropolis.py
@@ -61,7 +61,7 @@ class AdaptiveMetropolis():
 
     def set_batch_covariance(self, Z):
         self.mu = np.mean(Z, axis=0)
-        self.L_C = np.linalg.cholesky(np.cov(Z.T)+np.eye(Z.shape[1])*self.gamma2)
+        self.L_C = np.linalg.cholesky(self.nu2*np.cov(Z.T)+np.eye(Z.shape[1])*self.gamma2)
     
     def update_scaling(self, accept_prob):
         # generate updating weight