7500f929169d59e7443e29a20cf4e501ab54144c
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-10-05 18:51:41 +0100

    refactoring

--- scripts/demo_banana.py
+++ scripts/demo_banana.py
@@ -11,7 +11,7 @@ from kameleon_rks.gaussian_rks import feature_map, sample_basis, \
     gamma_median_heuristic
 import matplotlib.pyplot as plt
 import numpy as np
-from scripts.mini_mcmc import mini_mcmc
+from kameleon_rks import mini_mcmc.mini_mcmc
 from tools.convergence_stats import min_ess
 
 
