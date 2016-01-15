8bff997711beb814acb1f47df206e054c51092e4
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-01-15 17:34:38 +0000

    added adaptive metropolis (working) and kameleon_static (not working)

--- kameleon_rks/examples/demo_banana_all_proposals.py
+++ kameleon_rks/examples/demo_banana_all_proposals.py
@@ -1,11 +1,13 @@
-from kameleon_rks.densities.banana import log_banana_pdf
+from kameleon_rks.densities.banana import log_banana_pdf, sample_banana
 from kameleon_rks.examples.plotting import visualise_trace
 from kameleon_rks.mcmc.mini_mcmc import mini_mcmc
+from kameleon_rks.proposals.Kameleon import StaticKameleon
 from kameleon_rks.proposals.Metropolis import StaticMetropolis,\
     AdaptiveMetropolis
 from kameleon_rks.tools.log import Log
 import matplotlib.pyplot as plt
 import numpy as np
+from old.gaussian_rks import gamma_median_heuristic
 
 
 def sqrt_schedule(t):
@@ -30,6 +32,21 @@ def get_AdaptiveMetropolis_instance(D, target_log_pdf):
     
     return instance
 
+def get_StaticKameleon_instance(D, target_log_pdf):
+    
+    step_size = .01
+    schedule = sqrt_schedule
+    acc_star = 0.234
+    gamma2 = 0.1
+
+    Z = sample_banana(N=300, D=D, bananicity=0.03, V=100)
+    
+    kernel_sigma = 1./gamma_median_heuristic(Z)
+    instance = StaticKameleon(D, target_log_pdf, kernel_sigma, step_size, gamma2, schedule, acc_star)
+    instance.set_batch(Z)
+    
+    return instance
+
 if __name__ == '__main__':
     Log.set_loglevel(20)
     D = 2
@@ -41,6 +58,7 @@ if __name__ == '__main__':
     samplers = [
                 get_StaticMetropolis_instance(D, target_log_pdf),
                 get_AdaptiveMetropolis_instance(D, target_log_pdf),
+                get_StaticKameleon_instance(D, target_log_pdf), # not yet working
                 
                 ]
 
