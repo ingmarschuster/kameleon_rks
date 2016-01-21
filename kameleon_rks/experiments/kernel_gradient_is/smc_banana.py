fa0b6935b07438c8d1839e436e8b0b5abb339ab2
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-01-21 17:35:09 +0000

    smc_banana_experiment now with result storing

--- kameleon_rks/experiments/kernel_gradient_is/smc_banana.py
+++ kameleon_rks/experiments/kernel_gradient_is/smc_banana.py
@@ -1,13 +1,17 @@
+import os
+import time
+
 from kameleon_rks.densities.banana import log_banana_pdf, sample_banana
 from kameleon_rks.densities.gaussian import mvnorm
-from kameleon_rks.examples.plotting import visualize_scatter
+from kameleon_rks.experiments.tools import store_results
 from kameleon_rks.proposals.Langevin import AdaptiveLangevin, \
-    OracleKernelAdaptiveLangevin, KernelAdaptiveLangevin
+    OracleKernelAdaptiveLangevin
 from kameleon_rks.proposals.Metropolis import AdaptiveMetropolis
 from kameleon_rks.samplers.mini_smc import mini_smc
+from kameleon_rks.tools.convergence_stats import mmd_to_benchmark_sample, \
+    min_ess
 from kameleon_rks.tools.log import Log
 from kernel_exp_family.estimators.lite.gaussian import KernelExpLiteGaussian
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -32,7 +36,6 @@ def get_AdaptiveLangevin_instance(D, target_log_pdf, grad):
     gamma2 = 0.1
     
     instance = AdaptiveLangevin(D, target_log_pdf, grad, step_size, gamma2, schedule, acc_star)
-    instance.fixed_step_size = True
     
     return instance
 
@@ -43,61 +46,87 @@ def get_OracleKernelAdaptiveLangevin_instance(D, target_log_pdf, grad):
     acc_star = 0.574
     N = 500
     gamma2 = 0.1
-    Z = sample_banana(N=N, D=D, bananicity=0.03, V=100)
     
     surrogate = KernelExpLiteGaussian(sigma=10, lmbda=0.001, D=D, N=N)
-    surrogate.fit(Z)
     
     instance = OracleKernelAdaptiveLangevin(D, target_log_pdf, N, surrogate, step_size, gamma2, schedule, acc_star)
-    instance.fixed_step_size = True
-    
-    return instance
-
-def get_KernelAdaptiveLangevin_instance(D, target_log_pdf, grad):
-    step_size = 1.
-    schedule = lambda t : 1. / (t + 1) ** 0.75
-    acc_star = 0.574
-    gamma2 = 0.1
-    n = 500
-    
-    surrogate = KernelExpLiteGaussian(sigma=10, lmbda=0.001, D=D, N=n)
-    instance = KernelAdaptiveLangevin(D, target_log_pdf, n, surrogate, step_size, gamma2, schedule, acc_star)
-    instance.fixed_step_size = True
     
     return instance
 
 if __name__ == '__main__':
-    Log.set_loglevel(10)
+    Log.set_loglevel(20)
     D = 2
     
-    bananicity = 0.03
+    bananicity = 0.1
     V = 100
+    true_mean = np.zeros(D)
+    true_var = np.ones(D)
+    true_var[0] = 100
+    true_var[1] = 200
+    benchmark_sample = sample_banana(5000, D, bananicity, V)
     
     target_log_pdf = lambda x: log_banana_pdf(x, bananicity, V, compute_grad=False)
-    grad = lambda x: log_banana_pdf(x, bananicity, V, compute_grad=True)
+    target_grad = lambda x: log_banana_pdf(x, bananicity, V, compute_grad=True)
 
     samplers = [
                 get_AdaptiveMetropolis_instance(D, target_log_pdf),
-                get_AdaptiveLangevin_instance(D, target_log_pdf, grad),
-                get_OracleKernelAdaptiveLangevin_instance(D, target_log_pdf, grad),
-                get_KernelAdaptiveLangevin_instance(D, target_log_pdf, grad),
+                get_AdaptiveLangevin_instance(D, target_log_pdf, target_grad),
+                get_OracleKernelAdaptiveLangevin_instance(D, target_log_pdf, target_grad),
                 ]
     
     for sampler in samplers:
-        bridge_start = mvnorm(np.zeros(D), np.eye(D) * 10)
+        bridge_start_var = 10
+        bridge_start = mvnorm(np.zeros(D), np.eye(D) * bridge_start_var)
+        
+        targ_ef_bridge = 0.5
+        targ_ef_stop = 0.9
+        ef_tolerance = 0.02
         
         num_population = 1000
         num_samples = num_population
+        
+        start_time = time.time()
         samples, log_target_densities, step_sizes, acceptance_rates, evid = mini_smc(num_samples,
                                                               num_population,
                                                               bridge_start,
                                                               target_log_pdf,
                                                               sampler,
-                                                              targ_ef_bridge=0.8)
-        visualize_scatter(samples, step_sizes, acceptance_rates)
+                                                              targ_ef_bridge=targ_ef_bridge,
+                                                              targ_ef_stop=targ_ef_stop,
+                                                              ef_tolerance=ef_tolerance,
+                                                              )
+        time_taken = time.time() - start_time
         
-        plt.suptitle("%s" % \
-                     (sampler.__class__.__name__))
+        mmd = mmd_to_benchmark_sample(samples, benchmark_sample, degree=3)
+        ess = min_ess(samples)
+        rmse_mean = np.linalg.norm(true_mean - np.mean(samples, 0))
+        rmse_var = np.linalg.norm(true_var - np.var(samples, 0))
+        norm_of_mean = np.linalg.norm(np.mean(samples, axis=0))
         
-    plt.show()
+        fname = os.path.splitext(os.path.basename(__file__))[0] + ".txt"
+        store_results(fname,
+                      sampler_name=sampler.__class__.__name__,
+                      D=D,
+                      bananicity=bananicity,
+                      V=V,
+                      bridge_start_var=bridge_start_var,
+                      mmd=mmd,
+                      ess=ess,
+                      rmse_mean=rmse_mean,
+                      rmse_var=rmse_var,
+                      norm_of_mean=norm_of_mean,
+                      time_taken=time_taken,
+                      num_population=num_population,
+                      targ_ef_bridge=targ_ef_bridge,
+                      targ_ef_stop=targ_ef_stop,
+                      ef_tolerance=ef_tolerance,
+                      )
+
+    
+#         visualize_scatter(samples, step_sizes, acceptance_rates)
+#         plt.suptitle("%s" % \
+#                      (sampler.__class__.__name__))
+#     
+#     if False:
+#         plt.show()
 
