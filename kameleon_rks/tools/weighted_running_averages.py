148adc7a77c73720abc34ee98fc4a09ca8b3c15e
ingmarschuster <ingmar.schuster.linguistics@gmail.com>
2016-01-27 11:01:47 +0100

    weighted cholesky updates work

--- kameleon_rks/tools/weighted_running_averages.py
+++ kameleon_rks/tools/weighted_running_averages.py
@@ -14,6 +14,7 @@ import scipy.stats as stats
 from numpy import exp, log, sqrt
 from scipy.misc import logsumexp
 from numpy.linalg import inv
+from choldate._choldate import cholupdate, choldowndate
 
 
 
@@ -38,7 +39,23 @@ def mean_cov_weighted(samps, weights, logspace = False):
     return (mu, cov, w_s)        
 
 
-def mean_cov_upd_weighted(old_mean, old_cov, old_w_s, samps, weights, logspace = False):
+def mean_cov_upd_weighted(old_mean, old_cov, old_w_s, samps, weights, logspace = False, is_Chol = False):
+    """
+    Parameters
+    ==========
+    old_mean   -    old mean
+    old_cov    -    old covariance/lower cholesky factor
+    old_w_s    -    old sum of weights (in logspace if logspace==True)
+    samps      -    new samples
+    weights    -    weights of new samples (in logspace if logspace==True)
+    logspace   -    weights in logspace if True
+    is_Chol    -    old_cov and return value are upper cholesky factors
+    
+    Returns
+    =======
+    new_mean   -    new mean
+    new_cov    -    new covariance matrix or lower cholesky factor
+    """
     if len(samps.shape) > len(weights.shape):
         newsh = list(weights.shape)
         newsh.extend([1]*int(len(samps.shape) - len(weights.shape)))
@@ -58,14 +75,23 @@ def mean_cov_upd_weighted(old_mean, old_cov, old_w_s, samps, weights, logspace =
     
     new_mean = old_mean*old_new_ratio + np.sum(samps*weights,0)
     w_samps = samps * np.sqrt(weights)
-    new_cov = ((old_cov + np.outer(old_mean, old_mean)) * old_new_ratio
-                        - np.outer(new_mean, new_mean)
-                        +  w_samps.T.dot(w_samps))
+    
+    if not is_Chol:
+        new_cov = ((old_cov + np.outer(old_mean, old_mean)) * old_new_ratio
+                            - np.outer(new_mean, new_mean)
+                            +  w_samps.T.dot(w_samps))
+    else:
+        new_cov = old_cov.copy()
+        cholupdate(new_cov, old_mean)
+        new_cov = new_cov * np.sqrt(old_new_ratio)
+        choldowndate(new_cov, new_mean)
+        for s in w_samps:
+            cholupdate(new_cov, s)
     return (new_mean, new_cov, new_w_s)
 
 
 def test():
-    s_d = 1.
+    s_d = 20.
     D = 2
     num_w1 = 550
     num_w2 = 550
@@ -73,35 +99,46 @@ def test():
     w = np.r_[np.ones(num_w1), 2*np.ones(num_w2)]
     assert(len(w) == (num_w1+num_w2))
     rvs = np.r_[rvs_w, rvs_w[-num_w2:]]
+    mean_true = rvs.mean(0)
+    cov_true = np.cov(rvs.T)
+    chol_true = sp.linalg.cholesky(cov_true, lower=True)
     
     rd_idx = np.random.permutation(num_w1+num_w2)
     (rvs_w, w) = (rvs_w[rd_idx], w[rd_idx])
     
     
-    
-    for logspace in (False, True):
-        if logspace:
-            weights = log(w)
-            w_s = logsumexp(weights)
-            abs_toler = 10**(-3)
-        else:
-            weights = w
-            w_s = w.sum()
-            abs_toler = 10**(-3)
-        mean_true = rvs.mean(0)
-        cov_true = np.cov(rvs.T)
-        
-        (mean_batch, cov_batch, ws_batch) = mean_cov_weighted(rvs_w, weights, logspace)
-        assert(np.allclose(mean_true, mean_batch,  atol=abs_toler) and
-               np.allclose(cov_true, cov_batch, atol=abs_toler)
-               and np.allclose(ws_batch, w_s, atol=abs_toler)
+    for chol in (False, True):
+        for logspace in (False, True):
+            print("Cholesky", chol, "logspace", logspace)
+            if logspace:
+                weights = log(w)
+                w_s = logsumexp(weights)
+                abs_toler = 5*10**(-2)
+            else:
+                weights = w
+                w_s = w.sum()
+                abs_toler = 5*10**(-2)
+            abs_toler = abs_toler * s_d
+            
+
+            
+            (mean_batch, cov_batch, ws_batch) = mean_cov_weighted(rvs_w, weights, logspace)
+            assert(np.allclose(mean_true, mean_batch,  atol=abs_toler) and
+                   np.allclose(cov_true, cov_batch, atol=abs_toler)
+                   and np.allclose(ws_batch, w_s, atol=abs_toler)
+                   )
+
+                   
+            start = 500
+            (mean_start, cov_start, ws_start) = mean_cov_weighted(rvs_w[:start], weights[:start], logspace)
+            if not chol:
+                truth = cov_true
+            else:
+                cov_start = sp.linalg.cholesky(cov_start, lower = True)
+                truth = chol_true
+            (mean_upd, cov_upd, ws_upd) = mean_cov_upd_weighted(mean_start, cov_start, ws_start, rvs_w[start:], weights[start:], logspace, chol)
+            assert(np.allclose(mean_true, mean_upd, atol=abs_toler) and
+               np.allclose(truth, cov_upd, atol=abs_toler)
+               and np.allclose(ws_upd, w_s, atol=abs_toler)
                )
-               
-        start = 500
-        (mean_start, cov_start, ws_start) = mean_cov_weighted(rvs_w[:start], weights[:start],logspace)
-        (mean_upd, cov_upd, ws_upd) = mean_cov_upd_weighted(mean_start, cov_start, ws_start, rvs_w[start:], weights[start:], logspace)
-        assert(np.allclose(mean_true, mean_upd, atol=abs_toler) and
-           np.allclose(cov_true, cov_upd, atol=abs_toler)
-           and np.allclose(ws_upd, w_s, atol=abs_toler)
-           )
     
