06ec59c5d505ca4cca548c1de6881b8ab0f04b0a
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-02-02 17:21:34 +0000

    add method to store samples consecutively into file

--- kameleon_rks/experiments/tools.py
+++ kameleon_rks/experiments/tools.py
@@ -1,15 +1,11 @@
 import os
 from os.path import expanduser
 from time import strftime, gmtime, sleep
+import numpy as np
 
 import pandas as pd
 
-
-def store_results(fname = expanduser("~") + os.sep  + "results.txt", **kwargs):
-    # add filename if only path is given
-    if fname[-1] == os.sep:
-        fname += "results.txt"
-    
+def _create_dir_if_not_exist(fname):
     # create result dir if wanted
     if os.sep in fname:
         try:
@@ -17,6 +13,33 @@ def store_results(fname = expanduser("~") + os.sep  + "results.txt", **kwargs):
             os.makedirs(directory)
         except OSError:
             pass
+
+def store_samples(samples, fname = expanduser("~") + os.sep  + "results.txt", **kwargs):
+    # add filename if only path is given
+    if fname[-1] == os.sep:
+        fname += "results.txt"
+    
+    _create_dir_if_not_exist(fname)
+    
+    # very crude protection against conflicting access from parallel processes
+    write_success = False
+    while not write_success:
+        try:
+            # append to file
+            f_handle = file(fname, 'a')
+            np.savetxt(f_handle, samples)
+            f_handle.close()
+            write_success = True
+        except IOError:
+            print("IOError writing to %s ... trying again in 1s." % fname)
+            sleep(1)
+
+def store_results(fname = expanduser("~") + os.sep  + "results.txt", **kwargs):
+    # add filename if only path is given
+    if fname[-1] == os.sep:
+        fname += "results.txt"
+    
+    _create_dir_if_not_exist(fname)
     
     # use current time as index for the dataframe
     current_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
