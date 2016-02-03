c4de79ce0d7e2b4beafcdbee7b5a17600561d4c9
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-02-03 11:09:53 +0000

    put in resutlts from run 3 (seems stable now), abut increase variance by a larger amount to see whether things change

--- kameleon_rks/experiments/kernel_gradient_is/pmc_sv_benchmark_samples.py
+++ kameleon_rks/experiments/kernel_gradient_is/pmc_sv_benchmark_samples.py
@@ -20,35 +20,47 @@ if __name__ == '__main__':
     def get_AdaptiveIndependentMetropolis_instance(D, target_log_pdf):
         gamma2 = 0.1
         
-#         # run 1
-#         proposal_mu = np.array([-0.17973298, 0.11796741, 0.86733172, 0.52834129, -3.32354247])
-#         proposal_var = np.array([ 0.01932092, 0.02373668, 0.02096583, 0.1566503 , 0.46316933])
+#         # run 1 (initial parameters from Ingmar's pilot runs)
+#         proposal_mu = np.array([ -0.17973298, 0.11796741, 0.86733172, 0.52834129, -3.32354247])
+#         proposal_var = np.array([  0.01932092, 0.02373668, 0.02096583, 0.1566503 , 0.46316933])
 #         proposal_L_C = np.diag(np.sqrt(proposal_var))
 #         proposal_L_C *= 2
 #         # resuts:
-#         # mean: [0.08820961, -0.03436691,  0.7178945,   0.54124475, -3.77499049]
-#         # var: [0.01514925,  0.01407534,  0.056966,    0.37502436,  0.5887545]
+#         # mean: array([ 0.08820961, -0.03436691,  0.7178945,   0.54124475, -3.77499049])
+#         # var: array([ 0.01514925,  0.01407534,  0.056966,    0.37502436,  0.5887545])
 #         # np.mean(var): 0.201
 #         # np.linalg.norm(mean): 3.882
 #         # ESS: not printed
 
-#         # run 2
-#         proposal_mu = np.array([ 0.08820961, -0.03436691, 0.7178945, 0.54124475, -3.77499049 ])
-#         proposal_var = np.array([ 0.01514925, 0.01407534, 0.056966, 0.37502436,  0.5887545 ])
+#         # run 2 (use parameters of run 1, inflate variance by 2)
+#         proposal_mu = np.array([ 0.08820961, -0.03436691, 0.7178945, 0.54124475, -3.77499049])
+#         proposal_var = np.array([ 0.01514925, 0.01407534, 0.056966, 0.37502436,  0.5887545])
 #         proposal_L_C = np.diag(np.sqrt(proposal_var))
 #         proposal_L_C *= 2
 #         # results:
-#         # mean: array([ 0.13025213, -0.07923937, 0.48663143, 0.17922614, -3.6706272 ])
+#         # mean: array([ 0.13025213, -0.07923937, 0.48663143, 0.17922614, -3.6706272])
 #         # var: array([ 0.00990741, 0.01101477, 0.1076653 , 0.6019245 , 0.58744003])
 #         # np.mean(var): 0.264
 #         # np.linalg.norm(mean): 3.710
 #         # ESS: 2.702
         
-        # run 3
-        proposal_mu = np.array([ 0.13025213, -0.07923937, 0.48663143, 0.17922614, -3.6706272 ])
-        proposal_var = np.array([ 0.00990741, 0.01101477, 0.1076653 , 0.6019245 , 0.58744003])
+#         # run 3 (use parameters of run 2, inflate variance by 2)
+#         proposal_mu = np.array([ 0.13025213, -0.07923937, 0.48663143, 0.17922614, -3.6706272])
+#         proposal_var = np.array([ 0.00990741, 0.01101477, 0.1076653 , 0.6019245 , 0.58744003])
+#         proposal_L_C = np.diag(np.sqrt(proposal_var))
+#         proposal_L_C *= 2
+#         # results:
+#         # mean: array([ 0.14816278, -0.10084356,  0.34308655, -0.19531029, -3.58675556])
+#         # var: array([ 0.01056904,  0.0128013 ,  0.10549478,  0.56121843,  0.50030183])
+#         # np.mean(var): 0.238
+#         # np.linalg.norm(mean): 3.613
+#         # ESS: 5.031
+        
+        # run 4 (use parameters of run 3, inflate variance by 5)
+        proposal_mu = np.array([ 0.14816278, -0.10084356, 0.34308655, -0.19531029, -3.58675556])
+        proposal_var = np.array([ 0.01056904, 0.0128013 , 0.10549478, 0.56121843, 0.50030183])
         proposal_L_C = np.diag(np.sqrt(proposal_var))
-        proposal_L_C *= 2
+        proposal_L_C *= 5
         
         instance = AdaptiveIndependentMetropolis(D, target_log_pdf, gamma2, proposal_mu, proposal_L_C)
         
