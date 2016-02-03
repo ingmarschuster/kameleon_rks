25c25483c5fdc99eb28961f255bfd02efa9c3d5d
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-02-03 10:40:21 +0000

    first set of intermediade scalings, and some output changes

--- kameleon_rks/experiments/kernel_gradient_is/pmc_sv_benchmark_samples.py
+++ kameleon_rks/experiments/kernel_gradient_is/pmc_sv_benchmark_samples.py
@@ -20,12 +20,22 @@ if __name__ == '__main__':
     def get_AdaptiveIndependentMetropolis_instance(D, target_log_pdf):
         gamma2 = 0.1
         
-        # these parameters have been estimated from previous runs
-        proposal_mu = np.array([-0.17973298, 0.11796741, 0.86733172, 0.52834129, -3.32354247])
-        proposal_var = np.array([ 0.01932092, 0.02373668, 0.02096583, 0.1566503 , 0.46316933])
-        proposal_L_C = np.diag(np.sqrt(proposal_var))
-        
+#         # run 1
+#         proposal_mu = np.array([-0.17973298, 0.11796741, 0.86733172, 0.52834129, -3.32354247])
+#         proposal_var = np.array([ 0.01932092, 0.02373668, 0.02096583, 0.1566503 , 0.46316933])
+#         # resuts:
+#         # mean: [0.08820961, -0.03436691,  0.7178945,   0.54124475, -3.77499049]
+#         # var: [0.01514925,  0.01407534,  0.056966,    0.37502436,  0.5887545]
+#         # np.mean(var): 0.201
+#         # np.linalg.norm(mean): 3.882
+#         # ESS: not printed
+
+        # run 2
+        proposal_mu = np.array([0.08820961, -0.03436691,  0.7178945,   0.54124475, -3.77499049])
+        proposal_var = np.array([0.01514925,  0.01407534,  0.056966,    0.37502436,  0.5887545])
+
         # larger support for proposal
+        proposal_L_C = np.diag(np.sqrt(proposal_var))
         proposal_L_C *= 2
         
         instance = AdaptiveIndependentMetropolis(D, target_log_pdf, gamma2, proposal_mu, proposal_L_C)
@@ -57,8 +67,13 @@ if __name__ == '__main__':
     logger.info("Storing results under %s" % result_fname)
     store_samples(samples, result_fname)
     
-    print "mean:\n", np.mean(samples, axis=0)
-    print "var:\n", np.var(samples, axis=0)
+    mean = np.mean(samples, axis=0)
+    var = np.var(samples, axis=0)
+    print "mean:\n", repr(mean)
+    print "var:\n", repr(var)
+    print "np.mean(var): %.3f" % np.mean(var)
+    print "np.linalg.norm(mean): %.3f" % np.linalg.norm(mean)
+    print "ESS:", sampler.get_current_ess()
         
     if True:
         import matplotlib.pyplot as plt
